cd3d5a0c8c464b5fb7346879ff0797708abe58b5
Serina Tan <serinatan@cs.toronto.edu>
2019-05-02 15:26:30 -0400

    disable the "only run first kernel per stream" feature for now

--- src/gpgpusim_entrypoint.cc
+++ src/gpgpusim_entrypoint.cc
@@ -136,18 +136,18 @@ void *gpgpu_sim_thread_concurrent(void*)
             if(g_stream_manager->operation(&sim_cycles) && !g_the_gpu->active())
                 break;
 
-            if (g_the_gpu->config_finish_first_kernel()
-            		&& g_stream_manager->all_stream_done_one_kernel()) {
-            	// this is a small hack in the simulator to kill the simulation
-            	// as along as we have each kernel finishes the first run
-            	// when one kernel completes before the other kernel,
-            	// we will continue launching that kernel instance in that stream
-            	// to make sure we get the concurrent kernel performance
-
-            	// this ignores the kernel launched in default stream if any
-            	g_stream_manager->cancel_remaining_kernels();
-                done_one_kernel = true;
-            }
+//            if (g_the_gpu->config_finish_first_kernel()
+//            		&& g_stream_manager->all_stream_done_one_kernel()) {
+//            	// this is a small hack in the simulator to kill the simulation
+//            	// as along as we have each kernel finishes the first run
+//            	// when one kernel completes before the other kernel,
+//            	// we will continue launching that kernel instance in that stream
+//            	// to make sure we get the concurrent kernel performance
+//
+//            	// this ignores the kernel launched in default stream if any
+//            	g_stream_manager->cancel_remaining_kernels();
+//                done_one_kernel = true;
+//            }
 
             //functional simulation
             if( g_the_gpu->is_functional_sim()) {
