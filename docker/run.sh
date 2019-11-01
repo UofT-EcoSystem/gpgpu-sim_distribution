34f6606f404689a64b93263222b898b46855fe47
Serina Tan <serinatan@cs.toronto.edu>
2019-11-01 17:15:08 -0400

    Remove serinatan dependency in dockerfile

--- docker/run.sh
+++ docker/run.sh
@@ -1,4 +1,4 @@
-docker build . -t gpusim
+docker build . -t gpusim --build-arg user=$USER --build-arg userid=`id -u` && \
 docker run --privileged --hostname docker.gpusim -dit \
   -e DISPLAY=$DISPLAY \
   --runtime=nvidia -v `pwd`/../../:/mnt --ipc=host --name gpusim gpusim
