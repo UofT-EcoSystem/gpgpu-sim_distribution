02e374f3922089eb12b662f4f0e3f10ab7f4f9d1
Serina Tan <serinatam0812@gmail.com>
2020-05-15 13:27:32 -0400

    Regression: register print was broken after previous stringstream fix. Use sprintf and then push through stringstream seems to work.

--- src/cuda-sim/instructions.cc
+++ src/cuda-sim/instructions.cc
@@ -212,8 +212,14 @@ void ptx_thread_info::print_reg_thread(char * fname)
 
 void ptx_thread_info::print_reg_thread_strbuf(char *& buf)
 {
-    // Avoid c char array overflow, use stringstream and then copy
-	std::stringstream ss;
+    // Avoid c char overflow, use stringstream and then copy
+    std::stringstream ss;
+
+//	const unsigned buf_size = 16384;
+//	buf = new char[buf_size];
+//	memset(buf, 0, buf_size);
+//	int length = 0;
+
 	int size = m_regs.size();
 
 	if(size>0)
@@ -225,25 +231,26 @@ void ptx_thread_info::print_reg_thread_strbuf(char *& buf)
 		{
 			const std::string &name = it->first->name();
 			const std::string &dec= it->first->decl_location();
-			unsigned sizeBytes = it->first->get_size_in_bytes();
+			unsigned size = it->first->get_size_in_bytes();
 
-//			length += snprintf(buf+length, buf_size-length, "%s %llu %s %d\n",name.c_str(),it->second,
-//			        dec.c_str(),sizeBytes );
+			const unsigned buf_size = 256;
+			char buffer[buf_size];
 
-            char reg_union[256];
-            snprintf(reg_union, 256, "%llu", it->second);
+			memset(buffer, 0, buf_size);
+			int length = snprintf(buffer, buf_size, "%s %llu %s %d\n",
+			        name.c_str(), it->second, dec.c_str(),size );
+			assert(length < buf_size);
 
-            ss << name.c_str() << " " << reg_union << " " << dec.c_str() << " " << sizeBytes << "\n";
-
+			ss << buffer;
 		}
+
 		//m_regs.pop_back();
 	}
 
-	// +1 to include the null terminating string
-    const unsigned buf_size = std::strlen(ss.str().c_str()) + 1;
-    buf = new char[buf_size];
-    memset(buf, 0, buf_size);
-    std::strcpy(buf, ss.str().c_str());
+	const unsigned total_size = std::strlen(ss.str().c_str()) + 1;
+	buf = new char[total_size];
+	memset(buf, 0, total_size);
+	std::strcpy(buf, ss.str().c_str());
 }
 
 void ptx_thread_info::resume_reg_thread(char * fname, symbol_table * symtab)
